4742dcdf4d7b8a35ca2f6a2de0ecde24d257e1e2
brendan <2bndy5@gmail.com>
2020-11-13 02:22:50 -0800

    add RF24, RF24Network, RF24Mesh as submodules & copied wrappers to pkg folder

--- setup.py
+++ setup.py
@@ -1,17 +1,61 @@
+import os
+import sys
 import setuptools
+import crossunixccompiler
 
 long_description = ""
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+
+def process_configparams():
+
+    with open("../Makefile.inc") as f:
+        config_lines = f.read().splitlines()
+
+    cflags = os.getenv("CFLAGS", "")
+    for line in config_lines:
+        identifier, value = line.split("=", 1)
+        if identifier == "CPUFLAGS":
+            cflags += " " + value
+        elif identifier == "HEADER_DIR":
+            cflags += " -I" + os.path.dirname(value)
+        elif identifier == "LIB_DIR":
+            cflags += " -L" + value
+        elif identifier in ("CC", "CXX"):
+            os.environ[identifier] = value
+
+    os.environ["CFLAGS"] = cflags
+
+if sys.version_info >= (3,):
+    BOOST_LIB = "boost_python3"
+else:
+    BOOST_LIB = "boost_python"
+
+process_configparams()
+crossunixccompiler.register()
+
+module_RF24 = setuptools.Extension(
+    "RF24", libraries=["rf24", BOOST_LIB], sources=["pyRF24.cpp"]
+)
+
+module_RF24Network = setuptools.Extension(
+    "RF24Network", libraries=["rf24network", BOOST_LIB], sources=["pyRF24Network.cpp"]
+)
+
+module_RF24Mesh = setuptools.Extension(
+    "RF24Mesh",
+    libraries=["rf24mesh", "rf24network", BOOST_LIB],
+    sources=["pyRF24Mesh.cpp"],
+)
 setuptools.setup(
     name="pyrf24",
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     author="TMRh20 Avamander mz-fuzzy haseebehsan wamonite 2bndy5",
     author_email="tmrh20@gmail.com avamander@gmail.com "
-                 "mzfuzzy800@gmail.com fa14-bse-081@student.comsats.edu.pk "
-                 "2bndy5@gmail.com",
+    "mzfuzzy800@gmail.com fa14-bse-081@student.comsats.edu.pk "
+    "2bndy5@gmail.com",
     description="A python package for the wrapping RF24 related C++ libraries",
     long_description=long_description,
     long_description_content_type="text/x-rst",
