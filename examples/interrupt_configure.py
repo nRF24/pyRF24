8b4aa48ad7f274eac26c3e755399930991f06418
Brendan <2bndy5@gmail.com>
2024-03-24 18:44:41 -0700

    update irq example
    switch from RPi.GPIO to gpiod

--- examples/interrupt_configure.py
+++ examples/interrupt_configure.py
@@ -3,14 +3,32 @@ Simple example of detecting (and verifying) the IRQ (interrupt) pin on the
 nRF24L01
 """
 
+import argparse
+import select
 import sys
 import time
-import RPi.GPIO as GPIO
+
+try:
+    import gpiod
+    from gpiod.line import Edge
+except ImportError as exc:
+    raise ImportError(
+        "This script requires gpiod installed for observing the IRQ pin. Please run\n"
+        "\n    pip install gpiod\n\nMore details at https://pypi.org/project/gpiod/"
+    ) from exc
 from pyrf24 import RF24, RF24_PA_LOW, RF24_DRIVER
 
-# RPi.GPIO will show a warning if any pin is setup() that is already been
-# setup() for use without calling cleanup() first
-GPIO.cleanup()  # call this now in case it wasn't called on last program exit
+try:  # try RPi5 gpio cjip first
+    chip_path = "/dev/gpiochip4"
+    chip = gpiod.Chip(chip_path)
+except FileNotFoundError:  # fall back to gpio chip for RPi4 or older
+    chip_path = "/dev/gpiochip0"
+    chip = gpiod.Chip(chip_path)
+finally:
+    # print gpio chip info
+    info = chip.get_info()
+    print("Using {} [{}] ({} lines)".format(info.name, info.label, info.num_lines))
+
 
 ########### USER CONFIGURATION ###########
 # See https://github.com/TMRh20/RF24/blob/master/pyRF24/readme.md
@@ -29,7 +47,7 @@ else:
 radio = RF24(CE_PIN, CSN_PIN)
 
 # select your digital input pin that's connected to the IRQ pin on the nRF24L01
-IRQ_PIN = 12
+IRQ_PIN = 24
 
 # For this example, we will use different addresses
 # An address need to be a buffer protocol object (bytearray)
@@ -75,7 +93,7 @@ ack_payloads = (b"Yak ", b"Back", b" ACK")
 
 def interrupt_handler():
     """This function is called when IRQ pin is detected active LOW"""
-    print("IRQ pin went active LOW.")
+    print("\tIRQ pin went active LOW.")
     tx_ds, tx_df, rx_dr = radio.what_happened()  # update IRQ status flags
     print(f"\ttx_ds: {tx_ds}, tx_df: {tx_df}, rx_dr: {rx_dr}")
     if pl_iterator[0] == 0 and rx_dr:
@@ -87,26 +105,27 @@ def interrupt_handler():
 
 
 # setup IRQ GPIO pin
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(IRQ_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(IRQ_PIN, GPIO.FALLING, callback=interrupt_handler)
+irq_line = gpiod.request_lines(
+    path=chip_path,
+    consumer="pyrf24/examples/interrupt_configure.py",  # optional
+    config={IRQ_PIN: gpiod.LineSettings(edge_detection=Edge.FALLING)},
+)
+poll = select.poll()
+poll.register(irq_line.fd, select.POLLIN)
 
 
-def _wait_for_irq():
-    """Wait till IRQ_PIN goes active then inactive.
+def _wait_for_irq(timeout: int = 15):
+    """Wait till IRQ_PIN goes active (LOW).
     IRQ pin is LOW when activated. Otherwise it is always HIGH
-
-    In this example, the "data fail" event is always configured to
-    trigger the IRQ pin active. Because the auto-ACK feature is on by
-    default, the timeout in GPIO.wait_for_edge() is inconsequential.
     """
-    # wait up to 1 second for event to be cleared.
-    # GPIO.RISING means whatHappened() was called.
-    channel = GPIO.wait_for_edge(IRQ_PIN, GPIO.RISING, timeout=1000)
-    if channel is None:  # channel should be equal to GPIO.HIGH
+    # wait up to ``timeout`` seconds for event to be detected.
+    result = poll.poll(timeout * 1000)  # pass `None` to block
+    if not result:  # channel should be equal to GPIO.HIGH
         raise RuntimeError("Interrupt event not detected. Check your wiring.")
-    # wait for half a second for interrupt_handler() to complete
-    time.sleep(0.5)
+    for event in irq_line.read_edge_events():
+        if event.line_offset == IRQ_PIN and event.event_type is event.Type.FALLING_EDGE:
+            return True
+    return False
 
 
 def master():
@@ -117,23 +136,26 @@ def master():
     # on data ready test
     print("\nConfiguring IRQ pin to only ignore 'on data sent' event")
     radio.mask_irq(True, False, False)  # args = tx_ds, tx_df, rx_dr
-    print("    Pinging slave node for an ACK payload...", end=" ")
+    print("    Pinging slave node for an ACK payload...")
     pl_iterator[0] = 0
     radio.start_write(tx_payloads[0], False)  # False means expecting an ACK
-    _wait_for_irq()
+    if _wait_for_irq():
+        interrupt_handler()
 
     # on "data sent" test
     print("\nConfiguring IRQ pin to only ignore 'on data ready' event")
     radio.mask_irq(False, False, True)  # args = tx_ds, tx_df, rx_dr
-    print("    Pinging slave node again...             ", end=" ")
+    print("    Pinging slave node again...")
     pl_iterator[0] = 1
     radio.start_write(tx_payloads[1], False)  # False means expecting an ACK
-    _wait_for_irq()
+    if _wait_for_irq():
+        interrupt_handler()
 
     # trigger slave node to exit by filling the slave node's RX FIFO
     print("\nSending one extra payload to fill RX FIFO on slave node.")
+    print("Disabling IRQ pin for all events.")
+    radio.mask_irq(True, True, True)  # args = tx_ds, tx_df, rx_dr
     if radio.write(tx_payloads[2]):
-        # when send_only parameter is True, send() ignores RX FIFO usage
         print("Slave node should not be listening anymore.")
     else:
         print("Slave node was unresponsive.")
@@ -141,11 +163,12 @@ def master():
     # on "data fail" test
     print("\nConfiguring IRQ pin to go active for all events.")
     radio.mask_irq(False, False, False)  # args = tx_ds, tx_df, rx_dr
-    print("    Sending a ping to inactive slave node...", end=" ")
+    print("    Sending a ping to inactive slave node...")
     radio.flush_tx()  # just in case any previous tests failed
     pl_iterator[0] = 2
     radio.start_write(tx_payloads[3], False)  # False means expecting an ACK
-    _wait_for_irq()
+    if _wait_for_irq():
+        interrupt_handler()
     radio.flush_tx()  # flush artifact payload in TX FIFO from last test
     # all 3 ACK payloads received were 4 bytes each, and RX FIFO is full
     # so, fetching 12 bytes from the RX FIFO also flushes RX FIFO
@@ -154,6 +177,11 @@ def master():
 
 def slave(timeout=6):  # will listen for 6 seconds before timing out
     """Only listen for 3 payload from the master node"""
+    # the "data ready" event will trigger in RX mode
+    # the "data sent" or "data fail" events will trigger when we
+    # receive with ACK payloads enabled (& loaded in TX FIFO)
+    print("\nDisabling IRQ pin for all events.")
+    radio.mask_irq(True, True, True)  # args = tx_ds, tx_df, rx_dr
     # setup radio to receive pings, fill TX FIFO with ACK payloads
     radio.write_ack_payload(1, ack_payloads[0])
     radio.write_ack_payload(1, ack_payloads[1])
@@ -171,10 +199,68 @@ def slave(timeout=6):  # will listen for 6 seconds before timing out
         print("Complete RX FIFO:", radio.read(15))
 
 
-print(
-    f"""\
-    {sys.argv[0]}\n\
-    Make sure the IRQ pin is connected to the RPi GPIO12\n\
-    Run slave() on receiver\n\
-    Run master() on transmitter"""
-)
+def set_role():
+    """Set the role using stdin stream. Timeout arg for slave() can be
+    specified using a space delimiter (e.g. 'R 10' calls `slave(10)`)
+
+    :return:
+        - True when role is complete & app should continue running.
+        - False when app should exit
+    """
+    user_input = (
+        input(
+            "*** Enter 'R' for receiver role.\n"
+            "*** Enter 'T' for transmitter role.\n"
+            "*** Enter 'Q' to quit example.\n"
+        )
+        or "?"
+    )
+    user_input = user_input.split()
+    if user_input[0].upper().startswith("R"):
+        slave(*[int(x) for x in user_input[1:2]])
+        return True
+    if user_input[0].upper().startswith("T"):
+        master(*[int(x) for x in user_input[1:2]])
+        return True
+    if user_input[0].upper().startswith("Q"):
+        radio.power = False
+        return False
+    print(user_input[0], "is an unrecognized input. Please try again.")
+    return set_role()
+
+
+print(sys.argv[0])  # print example name
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "-r",
+        "--role",
+        type=int,
+        choices=range(2),
+        help="'1' specifies the TX role. '0' specifies the RX role.",
+    )
+    args = parser.parse_args()  # parse any CLI args
+
+    try:
+        if args.role is None:  # if not specified with CLI arg '-r'
+            while set_role():
+                pass  # continue example until 'Q' is entered
+        elif bool(args.role):  # if role was set using CLI args, run role once and exit
+            master()
+        else:
+            slave()
+    except KeyboardInterrupt:
+        print(" Keyboard Interrupt detected. Exiting...")
+        radio.power = False
+        sys.exit()
+else:
+    print(
+        f"Make sure the IRQ pin is connected to the GPIO{IRQ_PIN}",
+        "Run slave() on receiver",
+        "Run master() on transmitter",
+        sep="\n",
+    )
